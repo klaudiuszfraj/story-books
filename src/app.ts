9327475bee4106c2e1f6951a0c3f7f3773697818
Klaudiusz Frajkowski <klaudiusz.fraj@gmail.com>
2021-07-03 16:09:03 +0200

    add conncetion to db + morgan logs

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import serverConfig from './config/confing';
+import {connectDB} from "./config/database";
+import morgan from "morgan";
 
-
+connectDB();
 const app = express();
+process.env.NODE_ENV === 'development' && app.use(morgan('dev'));
 
-const PORT = serverConfig.PORT || 3000;
+const PORT = serverConfig.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
