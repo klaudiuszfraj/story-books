7cad23744f19b34e4712a133a10d577c3d7dd446
Klaudiusz Frajkowski <klaudiusz.fraj@gmail.com>
2021-07-03 16:08:43 +0200

    config connection to db

--- src/config/confing.ts
+++ src/config/confing.ts
@@ -1,17 +1,21 @@
 import dotenv from 'dotenv';
 
 //load config
-dotenv.config({ path: __dirname + '/.env' });
+dotenv.config({ path: `${__dirname}/.env` });
 
-const {
-    PORT,
-    MONGO_URL
-} = process.env;
-
-console.log(PORT)
+// server options
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL || 'default database URL';
 
 console.assert(PORT, 'PORT is require');
 console.assert(MONGO_URL, 'MONGO_URL is require');
+
+// db options
+export const optionsDB = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}
 
 export default {
     PORT,
