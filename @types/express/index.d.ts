a82fdd49704145184c0ade557d7e05add37ad5c6
Klaudiusz Frajkowski <klaudiusz.fraj@gmail.com>
2021-07-04 17:22:49 +0200

    add working merge declarations

--- @types/express/index.d.ts
+++ @types/express/index.d.ts
@@ -1,13 +1,26 @@
-declare global{
-    namespace Express {
-        interface Request {
-            user: {
-                googleID: string;
-                displayName: string;
-                firstName: string;
-                lastName: string;
-                image: string;
-            }
+//todo:: ts error Property 'firstName' does not exist on type 'User'. Property 'firstName' does not exist on type 'User'.
+// declare global{
+//     namespace Express {
+//         interface Request {
+//             user: {
+//                 googleID: string;
+//                 displayName: string;
+//                 firstName: string;
+//                 lastName: string;
+//                 image: string;
+//             }
+//         }
+//     }
+// }
+
+declare namespace Express {
+    export interface Request {
+        user: {
+            googleID: string;
+            displayName: string;
+            firstName: string;
+            lastName: string;
+            image: string;
         }
     }
 }
