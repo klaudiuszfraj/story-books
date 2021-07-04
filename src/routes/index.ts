a373ff7f74e8d2468d41ad640f42b4216286634e
Klaudiusz Frajkowski <klaudiusz.fraj@gmail.com>
2021-07-04 11:01:01 +0200

    add middleware to protect routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,18 @@
 import express from "express";
+import {ensureAuth, ensureGuest} from "../middleware/auth";
 
 const router = express.Router();
 
 // login route
-router.get('/', ((req, res) => {
+router.get('/', ensureGuest, ((req, res) => {
     res.render('login', {
         layout: 'login'
     });
 }));
 
 //dashboard route
-router.get('/dashboard', ((req, res) => {
+router.get('/dashboard', ensureAuth, ((req, res) => {
+    console.log(req.user)
     res.render('dashboard');
 }));
 
